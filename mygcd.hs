ce0063d37413db60b055b1e18f9a8ef329c5ac04
alexhkurz <alexhkurz@gmail.com>
2019-10-29 21:35:39 -0700

    mygcd

--- mygcd.hs
+++ mygcd.hs
@@ -1,3 +1,8 @@
+// run `ghci` in the command line
+// then `:load mygcd.hs`
+// then `mygcd 12 18`
+// what happens for `mygcd 5 0?
+
 mygcd :: Int -> Int -> Int
 mygcd x y | x > y = mygcd (x-y) y
           | x < y = mygcd x (y-x)
